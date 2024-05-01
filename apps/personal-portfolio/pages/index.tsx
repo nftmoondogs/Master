37654395b93dc910f4ba2e1f69a5e04d5b9ed04a
nftmoondogs <nftmoondogs@gmail.com>
2024-05-01 19:25:42 +0530

    newcommit

--- apps/personal-portfolio/pages/index.tsx
+++ apps/personal-portfolio/pages/index.tsx
@@ -158,7 +158,7 @@ const LandingPage = () => {
                 </div>
                 <div className="col-md-6" data-aos="zoom-in">
               <div className="about-contact-box info-box shadow-box">
-                <Link className="overlay-link" href="https://pump.fun/board" />
+                <Link className="overlay-link" href="https://pump.fun/EqZ4yxAyqqJgr6o5WzrENfH8ep23YkJi3sZiFni7Jegm" />
                 <img src="/assets/bg1.png" alt="BG" className="bg-img" />
                 <img
                   src="/assets/icons/icon2.png"
